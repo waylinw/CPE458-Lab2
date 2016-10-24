84c71bb68c5de70b34df461daef93463460ce1ca
Peter Georgiev <georgiev2592@gmail.com>
2016-10-24 02:32:31 -0700

    server almost done

--- CoAp/server.py
+++ CoAp/server.py
@@ -1,6 +1,8 @@
 import socket
 import sys
 from struct import *
+import os
+from time import gmtime, strftime
 
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -10,13 +12,42 @@ server_address = ('localhost', 12345)
 print ('starting up on %s port %s' % server_address)
 sock.bind(server_address)
 
+success_ret_code = {'Created':0x00410000, 'Deleted':0x00420000, 'Valid':0x00430000, 'Changed':0x00440000, 'Content':0x00450000}
+client_error_code = {'Bad Request':0x00800000, 'Unauthorized':0x00810000, 'Bad Option':0x00820000, 'Forbidden':0x00830000,
+                     'Not Found':0x00840000, 'Method Not Allowed':0x00850000, 'Not Acceptable':0x00860000,
+                     'Precondition Failed':0x008C0000, 'Request Entity Too Large':0x008D0000,
+                     'Unsupported Content-Format':0x008F0000}
+server_error_code = {0x00:'Internal Server Error', 0x01:'Not Implemented', 0x02:'Bad Gateway',
+                     0x03:'Service Unavailable', 0x04:'Gateway Timeout', 0x05:'Proxying Not Supported'}
+
+def find(name, path):
+    for root, dirs, files in os.walk(path):
+        if name in files:
+            return os.path.join(root, name)
+
 def GET(header, option_data):
     print("GET Request")
-    return 0
+
+    if find(option_data.decode() + '.html', os.getcwd()):
+        code = success_ret_code['Content']
+
+        with open(option_data.decode() + '.html', 'r') as myfile:
+            ret_option_data = myfile.read()
+    else:
+        code = success_ret_code['Not Found']
+        ret_option_data = 'Not Found'
+
+    return code, ret_option_data
 
 def POST(header, option_data):
     print("POST Request")
-    return 0
+
+    code = success_ret_code['Created']
+    filename = strftime("%H%M%S%d%m%Y", gmtime()) + '.html'
+    with open( filename, 'w') as myfile:
+        myfile.write(option_data)
+
+    return code, os.getcwd() + '/' + filename
 
 def PUT(header, option_data):
     print("PUT Request")
@@ -24,7 +55,16 @@ def PUT(header, option_data):
 
 def DELETE(header, option_data):
     print("DELETE Request")
-    return 0
+
+    if find(option_data.decode() + '.html', os.getcwd()):
+        code = success_ret_code['Deleted']
+        os.remove(os.getcwd() + option_data.decode() + '.html')
+        ret_option_data = 'Deleted'
+    else:
+        code = success_ret_code['Not Found']
+        ret_option_data = 'Not Found'
+
+    return code, ret_option_data
 
 options = {1: GET, 2: POST, 3: PUT, 4: DELETE}
 
@@ -36,23 +76,24 @@ def unpackMsg(data):
     size = calcsize('!IB')
     return unpack('!IB', data[:size]), data[size:]
 
-def packMsg():
-    pass
+def packMsg(header, ret_code, ret_msg):
+    return pack('!IB', (header[0] | ret_code), header[1]) + bytes(ret_msg, 'utf+8')
 
 def recvData():
     while True:
         print ('waiting for msg')
         data, address = sock.recvfrom(1280)
         print ('received %s bytes from %s' % (len(data), address))
-        header_option, option_data = unpackMsg(data)
+        return data, address
 
-        return_msg = handleReq(header_option, option_data)
+def sendData(data, address):
+    sent = sock.sendto(data, address)
+    print('sent %s bytes back to %s' % (sent, address))
 
-        if return_msg:
-            sent = sock.sendto(return_msg, address)
-            print('sent %s bytes back to %s' % (sent, address))
+data, address = recvData()
 
-def sendData():
-    pass
+header_option, option_data = unpackMsg(data)
 
-recvData()
+return_header, return_msg = handleReq(header_option, option_data)
+
+sendData(packMsg(header_option, return_header, return_msg), address)
