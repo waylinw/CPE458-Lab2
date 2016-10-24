92b88c943ed040b71f6d0afc5e221a72d4a91550
Peter Georgiev <georgiev2592@gmail.com>
2016-10-23 23:54:33 -0700

    Unpacked data in server.py

--- CoAp/server.py
+++ CoAp/server.py
@@ -1,5 +1,6 @@
 import socket
 import sys
+from struct import *
 
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -9,11 +10,45 @@ server_address = ('localhost', 12345)
 print ('starting up on %s port %s' % server_address)
 sock.bind(server_address)
 
-while True:
-    print ('waiting for msg')
-    data, address = sock.recvfrom(1280)
-    print ('received %s bytes from %s' % (len(data), address))
-    print ('data is: ', data.decode())
-    if data:
-        sent = sock.sendto(data, address)
-        print('sent %s bytes back to %s' % (sent, address))
+def GET():
+    print("GET Request")
+
+def POST():
+    print("POST Request")
+
+def PUT():
+    print("PUT Request")
+
+def DELETE():
+    print("DELETE Request")
+
+options = {1: GET, 2: POST, 3: PUT, 4: DELETE}
+
+def handleReq(header_option):
+    code = (header_option[0] & 0x00FF0000) >> 16
+    options[code]()
+
+def unpackMsg(data):
+    size = calcsize('!IB')
+    return unpack('!IB', data[:size]), data[size:]
+
+def packMsg():
+    pass
+
+def recvData():
+    while True:
+        print ('waiting for msg')
+        data, address = sock.recvfrom(1280)
+        print ('received %s bytes from %s' % (len(data), address))
+        header_option, option_data = unpackMsg(data)
+
+        handleReq(header_option)
+
+        if data:
+            sent = sock.sendto(data, address)
+            print('sent %s bytes back to %s' % (sent, address))
+
+def sendData():
+    pass
+
+recvData()
